c1e58be7096a4cd37fc972ce0bd289604c0aeea8
olesya-volgina-interexy <olesya.volgina@interexy.com>
2026-07-07 11:54:47 +0300

    fix: added seed command to basic build to add admin user in deploy

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -10,7 +10,7 @@ async function main() {
 
   const user = await prisma.user.upsert({
     where: { email },
-    update: {},
+    update: { password: passwordHash },
     create: {
       email,
       password: passwordHash,
